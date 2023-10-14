e2659b786526fc11f74386540319ffb5a6e4b331
Ellen Jardon <e@ellens-mbp.lan>
2023-10-14 11:50:43 -0500

    completed bounus mission, still need to fix display of the menu of availalbe services from main part of the assignmnent.

--- src/main/java/org/launchcode/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/controllers/SpaDayController.java
@@ -27,7 +27,7 @@ public class SpaDayController {
     @GetMapping(value="")
     @ResponseBody
     public String customerForm () {
-        String html = "<form method = 'post'>" +
+        String html = "<form action='/menu' method = 'post'>" +
                 "Name: <br>" +
                 "<input type = 'text' name = 'name'>" +
                 "<br>Skin type: <br>" +
@@ -41,13 +41,14 @@ public class SpaDayController {
                 "<select name = 'manipedi'>" +
                 "<option value = 'manicure'>Manicure</option>" +
                 "<option value = 'pedicure'>Pedicure</option>" +
+                "<option value = 'both'>Both</option>" +
                 "</select><br>" +
                 "<input type = 'submit' value = 'Submit'>" +
                 "</form>";
         return html;
     }
 
-    @PostMapping(value="")
+    @PostMapping(value="/menu")
     public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
 
         ArrayList<String> facials = new ArrayList<>();
@@ -67,6 +68,15 @@ public class SpaDayController {
         model.addAttribute("name",name);
         model.addAttribute("skintype", skintype);
         model.addAttribute("manipedi", manipedi);
+
+        ArrayList<String> polishChoices = new ArrayList<>();
+
+        polishChoices.add("#FF33FD");
+        polishChoices.add("#FF3364");
+        polishChoices.add("#6133FF");
+
+        model.addAttribute("polishChoices", polishChoices);
+
 
         return "menu";
     }
